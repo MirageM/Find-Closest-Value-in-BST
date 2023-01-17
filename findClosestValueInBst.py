2afecda7a98a7c519c3328168a2c07a4aa7369ca
MirageM <imiragesz@gmail.com>
2023-01-16 22:34:08 -0500

    unfinished

---
+++ findClosestValueInBst.py
@@ -0,0 +1,8 @@
+abs(target - closest) > abs(target - tree.value):
+closest = tree.value
+target < tree.value:
+	return findClosestValue(tree.right, target, closest)
+if target > tree.value:
+	return findClosestValue(tree.left, target, closest)
+else:
+	return closest
